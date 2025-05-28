8c040b63758cf2222f50dae5a938ae59012d109c
Chris <71638505+DubSolid@users.noreply.github.com>
2025-05-28 13:18:58 +0200

    Add files via upload
    MADE IT OOOOOOP

--- install.py
+++ install.py
@@ -1,46 +1,59 @@
+from dataclasses import dataclass, field
 import subprocess
 import os
 import shutil
 
-def download_and_copy():
-    repo_name = "env_install"
-    url = "https://github.com/DubSolid/env_install"
 
-    nvim_target_directory = os.path.expanduser("~/.config/nvim")
-    tmux_target_directory = os.path.expanduser("~/.config/tmux")
+@dataclass
+class Path:
+    repo_name:  str = field(default_factory=lambda: "env_install")
+    url:        str = field(default_factory=lambda: "https://github.com/DubSolid/env_install")
+    repo_path:  str = field(default_factory=lambda: "./env_install")
 
-    repo_path = "./env_install"
-    nvim_source = os.path.join(repo_path, "nvim")
-    tmux_source = os.path.join(repo_path, "tmux")
+    nvim_source: str = field(init=False)
+    tmux_source: str = field(init=False)
+    nvim_target_directory: str = field(init=False)
+    tmux_target_directory: str = field(init=False) 
 
-    # TODO: Check if repo is already cloned correctly
-    if not os.path.exists(repo_name):
+    def __post_init__(self):
+        self.nvim_source = os.path.join(self.repo_path, "nvim")
+        self.tmux_source = os.path.join(self.repo_path, "tmux")
+        self.nvim_target_directory = os.path.expanduser("~/.config/nvim")
+        self.tmux_target_directory = os.path.expanduser("~/.config/tmux")
+    
+
+def download_repo(paths: Path):
+    if not os.path.exists(paths.repo_name):
         try:
-            subprocess.run(["git", "clone", url], check=True)
+            subprocess.run(["git", "clone", paths.url], check=True)
             print("Repository cloned successfully!")
         except subprocess.CalledProcessError as e:
             print(f"Failed to clone repo: {e}")
     else:
-        print(f"Repository '{repo_name}' already exists.")
+        print(f"Repository '{paths.repo_name}' already exists.")
 
-    if not os.path.exists(nvim_target_directory):
+
+def copy_files(paths: Path):
+    if not os.path.exists(paths.nvim_target_directory):
         try:
-            shutil.copytree(nvim_source, nvim_target_directory)
+            shutil.copytree(paths.nvim_source, paths.nvim_target_directory)
             print("Neovim files successfully copied to target path.")
         except Exception as e:
             print(f"Failed to copy nvim files: {e}")
     else:
-        print(f"Directory '{nvim_target_directory}' already exists.")
+        print(f"Directory '{paths.nvim_target_directory}' already exists.")
 
-    if not os.path.exists(tmux_target_directory):
+    if not os.path.exists(paths.tmux_target_directory):
         try:
-            shutil.copytree(tmux_source, tmux_target_directory)
+            shutil.copytree(paths.tmux_source, paths.tmux_target_directory)
             print("Tmux files successfully copied to target path.")
         except Exception as e:
             print(f"Failed to copy tmux files: {e}")
     else:
-        print(f"Directory '{tmux_target_directory}' already exists.")
+        print(f"Directory '{paths.tmux_target_directory}' already exists.")
+
 
 if __name__ == "__main__":
-    download_and_copy()
-
+    paths = Path()
+    download_repo(paths)
+    copy_files(paths)
