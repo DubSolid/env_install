12a3a76584aebdbe0307a8a497ca4afc62c25eea
Chris <71638505+DubSolid@users.noreply.github.com>
2025-05-28 12:36:05 +0200

    Add files via upload

--- install.py
+++ install.py
@@ -6,11 +6,10 @@ def download_and_copy():
     repo_name = "env_install"
     url = "https://github.com/DubSolid/env_install"
 
-    nvim_target_directory = "~/.config/nvim"
-    tmux_target_directory = "~/.config/tmux"
+    nvim_target_directory = os.path.expanduser("~/.config/nvim")
+    tmux_target_directory = os.path.expanduser("~/.config/tmux")
 
     repo_path = "./env_install"
-
     nvim_source = os.path.join(repo_path, "nvim")
     tmux_source = os.path.join(repo_path, "tmux")
     
@@ -21,25 +20,26 @@ def download_and_copy():
         except subprocess.CalledProcessError as e:
             print(f"Failed to clone repo: {e}")
     else:
-        print(f"Repository '{repo_name} already exists.")
+        print(f"Repository '{repo_name}' already exists.")
 
     if not os.path.exists(nvim_target_directory):
         try:
             shutil.copytree(nvim_source, nvim_target_directory)
             print("Neovim files successfully copied to target path.")
-        except subprocess.CalledProcessError as e:
+        except Exception as e:
             print(f"Failed to copy nvim files: {e}")
     else:
-        print(f"Directory '{nvim_target_directory} already exists.")
+        print(f"Directory '{nvim_target_directory}' already exists.")
 
     if not os.path.exists(tmux_target_directory):
         try:
             shutil.copytree(tmux_source, tmux_target_directory)
             print("Tmux files successfully copied to target path.")
-        except subprocess.CalledProcessError as e:
-            print("Failed to copy tmux files: {e}")
+        except Exception as e:
+            print(f"Failed to copy tmux files: {e}")
     else:
-        print(f"Directory '{nvim_target_directory} already exists.")
+        print(f"Directory '{tmux_target_directory}' already exists.")
 
 if __name__ == "__main__":
     download_and_copy()
+
